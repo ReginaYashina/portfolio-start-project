4630618c1e267f383521b7c26df60cefd60a6bac
Regina Yashina <reginayashina@inbox.ru>
2024-07-23 16:19:16 +0300

    feat: add styles for Header

---
+++ src/layout/header/headerMenu/headerMenu.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../../../styles/Theme';
+
+export const HeaderMenu = (props: { headerMenuItems: Array<string> }) => {
+  return (
+    <StyledHeaderMenu>
+      <ul>
+        {props.headerMenuItems.map((item: string, index: number) => {
+          return (
+            <ListItem key={index}>
+              <Link href={'#'}>
+                {item}
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+              </Link>
+            </ListItem>
+          );
+        })}
+      </ul>
+    </StyledHeaderMenu>
+  );
+};
+
+const StyledHeaderMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 25px;
+  }
+`;
+
+const Link = styled.a`
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 110%;
+  color: transparent;
+`;
+
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow: hidden;
+  /* outline: 1px solid red; */
+  color: ${theme.colors.accent};
+
+  & + & {
+    top: 50%;
+    span {
+      transform: translateY(-50%);
+      display: inline-block;
+    }
+  }
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  &:hover {
+    ${Mask} {
+      color: ${theme.colors.font};
+      transform: skewX(12deg) translateX(5px);
+
+      & + ${Mask} {
+        transform: skewX(12deg) translateX(-5px);
+      }
+    }
+
+    &:before {
+      transform: scale(1);
+    }
+  }
+  &::before {
+    content: '';
+    display: inline-block;
+    height: 3px;
+    background-color: ${theme.colors.accent};
+    position: absolute;
+    top: 50%;
+    left: -10px;
+    right: -10px;
+    z-index: 1;
+    transform: scale(0);
+  }
+`;
