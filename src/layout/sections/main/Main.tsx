f12d68ebf224a7f30007713bc3b42e24105bc93e
Regina Yashina <reginayashina@inbox.ru>
2024-07-24 14:58:14 +0300

    feat: add styles for Main Component

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,24 +4,47 @@ import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { stopCoverage } from 'v8';
 import { stderr } from 'process';
+import { Container } from '../../../components/Container';
+import { theme } from '../../../styles/Theme';
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper justify={'space-around'} align={'center'}>
-        <div>
-          <Descrription>Hi There</Descrription>
-          <Name>
-            I am <span>Regina Yashina</span>
-          </Name>
-          <MainTitle>A Web Developer</MainTitle>
-        </div>
-        <Photo src={photo} alt='photo' />
-      </FlexWrapper>
+      <Container>
+        <FlexWrapper justify={'space-between'} align={'center'}>
+          <div>
+            <Descrription>Hi There</Descrription>
+            <Name>
+              I am <span>Regina Yashina</span>
+            </Name>
+            <MainTitle>A Web Developer</MainTitle>
+          </div>
+          <PhotoWrapper>
+            <Photo src={photo} alt='photo' />
+          </PhotoWrapper>
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
 
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &:before {
+    content: '';
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+    display: block;
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
+`;
 const Photo = styled.img`
   width: 350px;
   height: 430px;
@@ -30,17 +53,34 @@ const Photo = styled.img`
 const StyledMain = styled.section`
   min-height: 100vh;
   background-color: #c3d3f2;
+  display: flex;
 `;
-
 const MainTitle = styled.h1`
   font-weight: 400;
   font-size: 27px;
 `;
-
 const Name = styled.h2`
+  font-family: 'Josefin Sans', sans-serif;
   font-weight: 700;
   font-size: 50px;
   letter-spacing: 0.05em;
+  margin: 10px 0;
+
+  & span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: '';
+      position: absolute;
+      display: inline-block;
+      bottom: 0;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      z-index: -1;
+    }
+  }
 `;
 const Descrription = styled.span`
   font-weight: 400;
--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -18,6 +18,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 	color:${theme.colors.font};
+	line-height: 1.2;
 }
 
 code {
