9861b1f82923145725981baf8411b5b12b0270de
Regina Yashina <reginayashina@inbox.ru>
2024-07-25 20:54:15 +0300

    feat: add styles for Footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Icon } from "../../components/icon/Icon";
 import { FlexWrapper } from "../../components/FlexWrapper";
+import { theme } from "../../styles/Theme";
 
 export const Footer = () => {
   return (
@@ -57,14 +58,43 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  min-height: 30vh;
-  background-color: #bcead0;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `;
-const Name = styled.span``;
+const Name = styled.span`
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 0.14em;
+`;
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  margin: 30px 0;
+  gap: 20px;
 `;
 const SocialItem = styled.li``;
-const SocialLink = styled.a``;
-const Copyright = styled.small``;
+const SocialLink = styled.a`
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  background-color: rgba(255, 255, 255, 0.1);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: ${theme.colors.accent};
+  cursor: pointer;
+
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    background-color: ${theme.colors.accent};
+    transform: translateY(-4px);
+  }
+`;
+const Copyright = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
+`;
