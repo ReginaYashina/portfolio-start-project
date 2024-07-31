d71421f7eb0524a8a0e1b441bbfcd8870dce3829
Regina Yashina <reginayashina@inbox.ru>
2024-07-31 14:45:22 +0300

    feat: add adaptive for Skills

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import { theme } from '../styles/Theme';
+import { font } from '../styles/Common';
 
 export const SectionTitle = styled.h2`
+  ${font({
+    family: "'Josefin Sans', sans-serif",
+    weight: 600,
+    Fmax: 36,
+    Fmin: 30,
+  })}
   text-align: center;
-  font-family: 'Josefin Sans', sans-serif;
-  font-weight: 600;
-  font-size: 36px;
   letter-spacing: 0.14em;
   position: relative;
   padding-bottom: 30px;
@@ -21,5 +25,8 @@ export const SectionTitle = styled.h2`
     left: 50%;
     transform: translateX(-50%);
     bottom: 0;
+  }
+  @media ${theme.media.mobile} {
+    padding-bottom: 24px;
   }
 `;
