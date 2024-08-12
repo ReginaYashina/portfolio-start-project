190822e546a871f9631624e4f4e22f03dc805e2b
Regina Yashina <reginayashina@inbox.ru>
2024-08-12 15:12:43 +0300

    feat: add send mail

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import { SectionTitle } from '../../../components/SectionTitle'
-import { Button } from '../../../components/Button'
-import { Container } from '../../../components/Container'
-import { S } from './Contacts_Styles'
+import React, { ElementRef, useRef } from "react";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Button } from "../../../components/Button";
+import { Container } from "../../../components/Container";
+import { S } from "./Contacts_Styles";
+import emailjs from "@emailjs/browser";
 
 export const Contacts: React.FC = () => {
+  const form = useRef<ElementRef<"form">>(null);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    if (!form.current) return;
+
+    emailjs
+      .sendForm("service_1bd5b1g", "template_o190b3m", form.current, {
+        publicKey: "6dwsKImNI9ooad8Pn",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
-    <S.Contacts id={'contact'}>
+    <S.Contacts id={"contact"}>
       <Container>
         <SectionTitle>Contact</SectionTitle>
-        <S.StyledForm>
-          <S.Field placeholder={'name'} />
-          <S.Field placeholder={'subject'} />
-          <S.Field placeholder={'message'} as={'textarea'} />
-          <Button type={'submit'}>Send message</Button>
+        <S.StyledForm ref={form} onSubmit={sendEmail}>
+          <S.Field required placeholder={"name"} name={"user_name"} />
+          <S.Field required placeholder={"email"} name={"email"} />
+          <S.Field required placeholder={"subject"} name={"subject"} />
+          <S.Field
+            required
+            placeholder={"message"}
+            as={"textarea"}
+            name={"message"}
+          />
+          <Button type={"submit"}>Send message</Button>
         </S.StyledForm>
       </Container>
     </S.Contacts>
-  )
-}
+  );
+};
