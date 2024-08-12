d190402bd3906d270a23c1b2dcae37d498c62705
Regina Yashina <reginayashina@inbox.ru>
2024-08-12 13:41:20 +0300

    feat: add animatiom for tabs

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,61 +1,64 @@
-import React from 'react'
-import { FlexWrapper } from '../../../components/FlexWrapper'
-import { SectionTitle } from '../../../components/SectionTitle'
-import { Skill } from './skill/Skill'
-import { Container } from '../../../components/Container'
-import { S } from './Skills_Styles'
+import React from "react";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Skill } from "./skill/Skill";
+import { Container } from "../../../components/Container";
+import { S } from "./Skills_Styles";
+import { Fade } from "react-awesome-reveal";
 
 const SkillData = [
   {
-    iconId: 'code',
-    title: 'html5',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "code",
+    title: "html5",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
   {
-    iconId: 'css',
-    title: 'css3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "css",
+    title: "css3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
   {
-    iconId: 'react',
-    title: 'React',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "react",
+    title: "React",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
   {
-    iconId: 'typescript',
-    title: 'typescript',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "typescript",
+    title: "typescript",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
   {
-    iconId: 'styled-components',
-    title: 'styled components',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "styled-components",
+    title: "styled components",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
   {
-    iconId: 'figma',
-    title: 'WEB DESIgN',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+    iconId: "figma",
+    title: "WEB DESIgN",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim",
   },
-]
+];
 
 export const Skills: React.FC = () => {
   return (
-    <S.Skills id={'skills'}>
+    <S.Skills id={"skills"}>
       <Container>
         <SectionTitle>My Skills</SectionTitle>
-        <FlexWrapper wrap={'wrap'} justify={'space-between'}>
-          {SkillData.map((s, index: number) => {
-            return (
-              <Skill
-                iconId={s.iconId}
-                title={s.title}
-                text={s.text}
-                key={index}
-              />
-            )
-          })}
+        <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+          <Fade cascade damping={0.2}>
+            {SkillData.map((s, index: number) => {
+              return (
+                <Skill
+                  iconId={s.iconId}
+                  title={s.title}
+                  text={s.text}
+                  key={index}
+                />
+              );
+            })}
+          </Fade>
         </FlexWrapper>
       </Container>
     </S.Skills>
-  )
-}
+  );
+};
--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,68 +1,113 @@
-import React, { useState } from 'react'
-import { SectionTitle } from '../../../components/SectionTitle'
-import { TabMenu, TabsStatusType } from './tabMenu/TabMenu'
-import { FlexWrapper } from '../../../components/FlexWrapper'
-import { Work } from './work/Work'
-import socialImg from '../../../assets/img/project-1.jpg'
-import timerImg from '../../../assets/img/project-2.jpg'
-import { Container } from '../../../components/Container'
-import { S } from './Works_Styles'
+import React, { useState } from "react";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { TabMenu, TabsStatusType } from "./tabMenu/TabMenu";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Work } from "./work/Work";
+import socialImg from "../../../assets/img/project-1.jpg";
+import timerImg from "../../../assets/img/project-2.jpg";
+import { Container } from "../../../components/Container";
+import { S } from "./Works_Styles";
+import { AnimatePresence, motion } from "framer-motion";
 
 const tabsItems: Array<{
-  status: 'all' | 'landing' | 'react' | 'spa'
-  title: string
+  status: "all" | "landing" | "react" | "spa";
+  title: string;
 }> = [
   {
-    title: 'All',
-    status: 'all',
+    title: "All",
+    status: "all",
   },
   {
-    title: 'Landing page',
-    status: 'landing',
+    title: "Landing page",
+    status: "landing",
   },
   {
-    title: 'React',
-    status: 'react',
+    title: "React",
+    status: "react",
   },
   {
-    title: 'SPA',
-    status: 'spa',
+    title: "SPA",
+    status: "spa",
   },
-]
+];
 
 const worksData = [
   {
     src: socialImg,
-    title: 'Social Network',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    type: 'spa',
+    title: "Social Network",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "spa",
+    id: 1,
   },
   {
     src: timerImg,
-    title: 'Timer',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    type: 'react',
+    title: "Timer",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "react",
+    id: 2,
   },
-]
+  {
+    src: socialImg,
+    title: "Social Network",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "spa",
+    id: 3,
+  },
+  {
+    src: timerImg,
+    title: "Timer",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "landing",
+    id: 4,
+  },
+  {
+    src: socialImg,
+    title: "Social Network",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "spa",
+    id: 5,
+  },
+  {
+    src: timerImg,
+    title: "Timer",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "react",
+    id: 6,
+  },
+  {
+    src: socialImg,
+    title: "Social Network",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "spa",
+    id: 7,
+  },
+  {
+    src: timerImg,
+    title: "Timer",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    type: "landing",
+    id: 8,
+  },
+];
 
 export const Works: React.FC = () => {
-  const [currentFilterStatus, setCurrentFilterStatus] = useState('all')
-  let filteredWorks = worksData
-  if (currentFilterStatus === 'landing') {
-    filteredWorks = worksData.filter((work) => work.type === 'landing')
+  const [currentFilterStatus, setCurrentFilterStatus] = useState("all");
+  let filteredWorks = worksData;
+  if (currentFilterStatus === "landing") {
+    filteredWorks = worksData.filter((work) => work.type === "landing");
   }
-  if (currentFilterStatus === 'react') {
-    filteredWorks = worksData.filter((work) => work.type === 'react')
+  if (currentFilterStatus === "react") {
+    filteredWorks = worksData.filter((work) => work.type === "react");
   }
-  if (currentFilterStatus === 'spa') {
-    filteredWorks = worksData.filter((work) => work.type === 'spa')
+  if (currentFilterStatus === "spa") {
+    filteredWorks = worksData.filter((work) => work.type === "spa");
   }
 
   function changeFilterStatus(value: TabsStatusType) {
-    setCurrentFilterStatus(value)
+    setCurrentFilterStatus(value);
   }
   return (
-    <S.Works id={'works'}>
+    <S.Works id={"works"}>
       <Container>
         <SectionTitle>My Works</SectionTitle>
         <TabMenu
@@ -71,18 +116,29 @@ export const Works: React.FC = () => {
           currentFilterStatus={currentFilterStatus}
         />
         <FlexWrapper
-          justify={'space-between'}
-          align={'flex-start'}
-          wrap={'wrap'}
-          gap={'30px'}
+          justify={"center"}
+          align={"flex-start"}
+          wrap={"wrap"}
+          gap={"30px"}
         >
-          {filteredWorks.map((w, index: number) => {
-            return (
-              <Work src={w.src} title={w.title} text={w.text} key={index} />
-            )
-          })}
+          <AnimatePresence>
+            {filteredWorks.map((w) => {
+              return (
+                <motion.div
+                  style={{ width: "400px", flexGrow: 1, maxWidth: "540px" }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  key={w.id}
+                  layout
+                >
+                  <Work src={w.src} title={w.title} text={w.text} key={w.id} />
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
         </FlexWrapper>
       </Container>
     </S.Works>
-  )
-}
+  );
+};
